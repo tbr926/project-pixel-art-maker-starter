a4fa52405cb172531c2b19e3634793ce51ec1b7e
tbr926 <107431559+tbr926@users.noreply.github.com>
2022-06-26 11:33:03 -0400

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,5 +1,14 @@
-// Select color input
-// Select size input
+// select color input
+const color = document.getElementById('colorPicker');
+// select size input
+const size = document.getElementById('sizePicker');
+// select canvas input
+const canvas = document.getElementById('pixelCanvas')
+// select height input
+const height = document.getElementById('inputheight').value;
+// select width input
+const width = document.getElementById('inputwidth').value;
+// when size is submitted by user, 
 
 // When size is submitted by the user, call makeGrid()
 
